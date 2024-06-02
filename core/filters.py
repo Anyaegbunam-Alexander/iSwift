40876a1c87af4c623086b2028e4de4eea6c4dacf
Alexander Anyaegbunam <alexanderking.aa@gmail.com>
2024-06-02 19:49:08 +0100

    add new filterset class

---
+++ core/filters.py
@@ -0,0 +1,17 @@
+from django.db.models import Q, QuerySet
+from django_filters import rest_framework as df_filters
+
+from accounts.models import User
+
+
+class UserFilter(df_filters.FilterSet):
+    search = df_filters.CharFilter(method="custom_filter")
+
+    class Meta:
+        model = User
+        fields = []  # We don't need to define any fields here since we are using a custom method
+
+    def custom_filter(self, queryset: QuerySet, name: str, value: str):
+        return queryset.filter(
+            Q(name__icontains=value) | Q(phone_number__icontains=value)
+        ).distinct()
