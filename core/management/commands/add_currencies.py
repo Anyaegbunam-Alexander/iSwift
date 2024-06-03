72ef63402339d776183bdcc24f7d938b39bcbab3
Alexander Anyaegbunam <alexanderking.aa@gmail.com>
2024-06-03 22:01:25 +0100

    add mgt command

--- core/management/commands/add_currencies.py
+++ core/management/commands/add_currencies.py
@@ -1,17 +1,15 @@
 from django.core.management.base import BaseCommand
 from django.db import transaction
 
-from core.data import currencies
+from finance.data import currencies
 from finance.models import Currency
-from finance.utils import update_rates
 
 
 class Command(BaseCommand):
-    help = "Create currencies if updated"
+    help = "Update currencies from dict"
 
     @transaction.atomic
     def handle(self, *args, **options):
-        # for key, value in currencies.items():
-        #     Currency.objects.get_or_create(name=value, iso_code=key)
-        update_rates()
+        for key, value in currencies.items():
+            Currency.objects.get_or_create(name=value, iso_code=key.lower())
         self.stdout.write(self.style.SUCCESS("Operation Success!!"))
