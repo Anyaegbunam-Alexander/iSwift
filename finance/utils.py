d02c837904fb1035739026db84dcb69d78c84c91
Alexander Anyaegbunam <alexanderking.aa@gmail.com>
2024-06-03 22:02:45 +0100

    add function to update currency

--- finance/utils.py
+++ finance/utils.py
@@ -1,38 +1,43 @@
 import logging
+from decimal import Decimal, getcontext
 
 import requests
-from django.conf import settings
 from django.db import transaction
 
 from finance.models import ConversionRate, Currency
 
 logger = logging.getLogger(__name__)
+getcontext().prec = 8
 
 
 def update_rates():
     currencies = Currency.objects.filter(status=Currency.ACTIVE_STATUS)
-    app_id = settings.OPEN_EXCHANGE_APP_ID
 
     with transaction.atomic():
         try:
             for i, base in enumerate(currencies):
-                url = f"https://openexchangerates.org/api/latest.json?app_id={app_id}&base={base.iso_code}&prettyprint=false"  # noqa
+                url = f"https://cdn.jsdelivr.net/npm/@fawazahmed0/currency-api@latest/v1/currencies/{base.iso_code}.json"  # noqa
                 response = requests.get(url, timeout=10)
-                print(response.json())
                 if response.status_code == 200:
+                    # fmt:off
                     for target in currencies[i + 1:]:
+                        # fmt:on
                         data = response.json()
-                        rate = data.get("rates", {}).get(target.iso_code)
-                        if rate and rate != 0:
-                            c_rate, _ = ConversionRate.objects.get_or_create(
-                                base_currency=base, target_currency=target
-                            )
-                            c_rate.conversion_rate = rate
-                            c_rate.reverse_rate = 1 / rate
-                            c_rate.save()
-                        else:
-                            logger.error(
-                                f"Invalid rate received for {base.iso_code} to {target.iso_code}"
-                            )
+                        rate = data.get(base.iso_code, {}).get(target.iso_code)
+                        if rate:
+                            rate_str = "{:.8f}".format(rate)
+                            rate_decimal = Decimal(rate_str)
+                            if rate_decimal != 0:
+                                c_rate, _ = ConversionRate.objects.get_or_create(
+                                    base_currency=base, target_currency=target
+                                )
+                                c_rate.conversion_rate = rate_decimal
+                                c_rate.reverse_rate = Decimal("1") / rate_decimal
+                                c_rate.save()
+                            else:
+                                logger.error(
+                                    f"Invalid rate received for {base.iso_code} to {target.iso_code}"  # noqa
+                                )
+
         except Exception as e:
-            logger.error(f"An error occurred: {e}")
+            print(e)
