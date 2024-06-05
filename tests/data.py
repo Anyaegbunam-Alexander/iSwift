31186aebbff97a69e272bd4078bfeee7a201364c
Alexander Anyaegbunam <alexanderking.aa@gmail.com>
2024-06-05 13:27:40 +0100

    add test data

--- tests/data.py
+++ tests/data.py
@@ -9,3 +9,66 @@ user_data = {
 }
 
 login_data = {"password": user_data["password"]}
+
+conversion_rates = [
+    {
+        "base_currency": "usd",
+        "target_currency": "cad",
+        "conversion_rate": "1.3635456100",
+        "reverse_rate": "0.7333821400",
+    },
+    {
+        "base_currency": "usd",
+        "target_currency": "gbp",
+        "conversion_rate": "0.7851128800",
+        "reverse_rate": "1.2737022000",
+    },
+    {
+        "base_currency": "usd",
+        "target_currency": "ngn",
+        "conversion_rate": "1398.9226508500",
+        "reverse_rate": "0.0007148358",
+    },
+    {
+        "base_currency": "usd",
+        "target_currency": "eur",
+        "conversion_rate": "0.9216418700",
+        "reverse_rate": "1.0850201000",
+    },
+    {
+        "base_currency": "cad",
+        "target_currency": "gbp",
+        "conversion_rate": "0.5757877700",
+        "reverse_rate": "1.7367510000",
+    },
+    {
+        "base_currency": "cad",
+        "target_currency": "ngn",
+        "conversion_rate": "1025.9448891700",
+        "reverse_rate": "0.0009747112",
+    },
+    {
+        "base_currency": "cad",
+        "target_currency": "eur",
+        "conversion_rate": "0.6759156900",
+        "reverse_rate": "1.4794745000",
+    },
+    {
+        "base_currency": "gbp",
+        "target_currency": "ngn",
+        "conversion_rate": "1781.8108499300",
+        "reverse_rate": "0.0005612268",
+    },
+    {
+        "base_currency": "gbp",
+        "target_currency": "eur",
+        "conversion_rate": "1.1738972700",
+        "reverse_rate": "0.8518633000",
+    },
+    {
+        "base_currency": "ngn",
+        "target_currency": "eur",
+        "conversion_rate": "0.0006588200",
+        "reverse_rate": "1517.8653000000",
+    },
+]
