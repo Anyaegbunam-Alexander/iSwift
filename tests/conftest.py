b9d7bbb9468f18659e19024d0013a09e312cb33c
Alexander Anyaegbunam <alexanderking.aa@gmail.com>
2024-06-05 13:27:29 +0100

    register new factories and fixtures

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,12 +1,27 @@
 from pytest_factoryboy import register
 
 from tests.factories.accounts import OTPFactory, UserFactory
-from tests.factories.finance import CurrencyFactory
+from tests.factories.finance import (
+    ConversionRateFactory,
+    CreditTransactionFactory,
+    CurrencyFactory,
+    DebitTransactionFactory,
+    iSwiftAccountFactory,
+)
 
-FACTORIES = [OTPFactory, UserFactory, CurrencyFactory]
+FACTORIES = [
+    OTPFactory,
+    UserFactory,
+    CurrencyFactory,
+    ConversionRateFactory,
+    iSwiftAccountFactory,
+    DebitTransactionFactory,
+    CreditTransactionFactory,
+]
 
 for i in FACTORIES:
     register(i)
 
 from tests.fixtures.anon_user import *  # noqa
 from tests.fixtures.auth_user import *  # noqa
+from tests.fixtures.finance import *  # noqa
