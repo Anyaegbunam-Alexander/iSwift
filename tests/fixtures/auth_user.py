cddf1ed155a89321929122190d87af4c0a5aee2f
Alexander Anyaegbunam <alexanderking.aa@gmail.com>
2024-06-05 13:29:35 +0100

    add new fixture

--- tests/fixtures/auth_user.py
+++ tests/fixtures/auth_user.py
@@ -3,7 +3,15 @@ from rest_framework.test import APIClient
 
 
 @pytest.fixture
-def auth_user_api_client(user_factory):
+def auth_client(user_factory):
     client = APIClient()
     client.force_authenticate(user_factory())
     return client
+
+
+@pytest.fixture
+def auth_user_client(user_factory):
+    client = APIClient()
+    user = user_factory()
+    client.force_authenticate(user)
+    return user, client
