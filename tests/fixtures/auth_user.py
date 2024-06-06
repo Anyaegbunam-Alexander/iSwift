cab6fbc8955b68433d14a03f7601bdf6b8379697
Alexander Anyaegbunam <alexanderking.aa@gmail.com>
2024-06-06 13:13:41 +0100

    add new fixture

--- tests/fixtures/auth_user.py
+++ tests/fixtures/auth_user.py
@@ -10,8 +10,20 @@ def auth_client(user_factory):
 
 
 @pytest.fixture
-def auth_user_client(user_factory):
+def auth_user_client(user_factory, iswift_account_factory):
     client = APIClient()
     user = user_factory()
+    iswift_account_factory(user=user, is_default=True)
     client.force_authenticate(user)
     return user, client
+
+
+@pytest.fixture
+def auth_user_with_iswift_accounts(user_factory, iswift_account_factory):
+    client = APIClient()
+    user = user_factory()
+    accounts = [iswift_account_factory(user=user, is_default=False) for _ in range(3)]
+    accounts[0].is_default = True
+    accounts[0].save()
+    client.force_authenticate(user)
+    return accounts, client
